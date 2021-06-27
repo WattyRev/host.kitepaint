f99a356daedde12e5ee52107a3580c3c8d2ebcac
Spencer Watson <wattyrev@gmail.com>
2021-06-27 13:26:54 -0700

    Tweaks

--- dist/static/index.php
+++ dist/static/index.php
@@ -1,10 +1,17 @@
 <?php
+function str_replace_first($from, $to, $content)
+{
+    $from = '/'.preg_quote($from, '/').'/';
+
+    return preg_replace($from, $to, $content, 1);
+}
+
 	// Get the domain
 	$domain = $_SERVER['SERVER_NAME'];
 
 	// Get the file path
   $requestUri = $_SERVER['REQUEST_URI'];
-  $filePath = str_replace('static/', '', $requestUri);
+  $filePath = str_replace_first('static/', '', $requestUri);
 
 	// Grab the relevant file from  stati.kitepaint.com
 	if ($domain === 'kitepaint.com') {
